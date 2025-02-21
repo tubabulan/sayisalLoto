b081fa2ad24ecad803709f7a9008449f72c1bff1
iegul <iremelfgl@gmail.com>
2025-02-21 12:47:53 +0300

    Initial Commit

---
+++ sayısal_loto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sayısal_loto
+//
+//  Created by Trakya12 on 21.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
